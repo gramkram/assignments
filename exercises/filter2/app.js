9168f64712f53338e791d9ebb072e0b48dad004d
Grant Kramer <grantkramer33@gmail.com>
2019-12-04 11:05:27 -0700

    12/4/19

--- exercises/filter2/app.js
+++ exercises/filter2/app.js
@@ -42,4 +42,6 @@ function peopleWhoBelongToTheIlluminati(arr){
     { name: "Paris Hilton", member: true },
     { name: "Kayne West", member: false },
     { name: "Bob Ziroll", member: true }
-]));
+])); 
+
+
---
+++ exercises/sort/app.js
@@ -0,0 +1,21 @@
+// const leastToGreatest = arr => {
+//     return arr.sort((a, b) => a - b)
+// }
+// // const results = [1, 34, 14, 48].sort((a, b) => a - b)
+// console.log(leastToGreatest([1, 3, 5, 2, 90, 20]));
+
+// const greatestToLeast = arr => {
+//     return arr.sort((a, b) => b - a)
+// }
+// const greatestToLeast = [1, 3, 5, 2, 90, 20].sort((a, b) => b - a)
+// console.log(greatestToLeast([1, 3, 5, 2, 90, 20]));
+// const lengthSort = arr => {
+//     return arr.sort((a, b) => a.length - b.length)
+// }
+
+// console.log(lengthSort(["dog", "wolf", "by", "family", "eaten"]))
+const alphabetical = arr => {
+    return arr.sort((a, b) => 
+}
+
+console.log(alphabetical(["dog", "wolf", "by", "family", "eaten"]));
